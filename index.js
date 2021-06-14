fee9a7277fbf45c6e5c54ade6f95d63b2e33b98e
Leonardo Alonso <leonardoalonsososa@gmail.com>
2021-06-14 17:41:50 -0500

    [ADD] add sockets

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.use(express.static( publicPath ));
 server.listen(process.env.PORT, (err) => {
     if (err) throw new Error(err);
 
-    console.log(`Servidor corriendo en puero`, process.env.PORT);
+    console.log(`Servidor corriendo en puerto`, process.env.PORT);
 });
