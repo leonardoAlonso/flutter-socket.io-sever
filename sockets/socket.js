bb4ed23f05dc2a61bb7227d341ca04a88ba255c9
= <=>
2021-06-24 21:13:35 -0500

    Add bands models and expose it on socket event

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,22 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('ska-p') )
+bands.addBand( new Band('metallica') )
+bands.addBand( new Band('nana pancha') )
+bands.addBand( new Band('rude boys') )
+
+console.log(bands)
 
 // Mensajes de sockets
 io.on('connection', client => {
     console.log("Cliente conectado");
 
+    client.emit('active-bands', bands)
 
     client.on('disconnect', () => {
         console.log("Cliente desconectado")
@@ -13,7 +26,11 @@ io.on('connection', client => {
     client.on('mensaje', (payload) => {
         console.log("Mensaje!!", payload);
         io.emit('mensaje', {admin: 'Nuevo mensaje' });
-    })
+    });
 
+    client.on('emitir-mensaje', (payload) => {
+	console.log("Flutter socket", payload)
+	client.broadcast.emit('nuevo-mensaje', payload);
+    });
 
 });
